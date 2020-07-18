376a666bda439eb89707dfdaf7788d553b808f10
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-07-18 19:52:00 +0900

    Added JQ to humberger and fixed a few views

--- app/assets/javascripts/_drawer.js
+++ app/assets/javascripts/_drawer.js
@@ -1,9 +1,5 @@
-//= require jquery
-//= require rails-ujs
-//= require_tree .
-
 $(function(){
-  $(".Header__logo").hover(function(){
+  $("li").hover(function(){
     $(this).animate({opacity: 0.6}, {duration: 100, easing: 'swing'})
   }, function(){
     $(this).animate({opacity: 1})
