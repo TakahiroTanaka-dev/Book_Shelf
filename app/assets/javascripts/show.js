211fd64270fbe2bd4d6fe4fbd54bbbcb2bc7e633
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-09-26 23:18:52 +0900

    詳細ページに編集機能を仮追加

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,5 +1,7 @@
 $(function(){
   $(document).on('click','.View__descriptions__edit', function(){
-    
+    // 編集ボタンを押したら、元あるhtmlを隠す
+    // 編集画面の入力できる部分とかを出す
+    // 更新ボタンを押したら更新されたhtmlを表示する
   })
 })
